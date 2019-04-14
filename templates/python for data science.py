336e3afded56d692bb2aed23256bcfaec031f357
Albert <albertsanchezlafuente@gmail.com>
2019-04-14 23:15:04 +0200

    reordered steps

--- templates/python for data science.py
+++ templates/python for data science.py
@@ -253,33 +253,6 @@ for train_index, val_index in skf.split(X, y):
 	X_train, X_val = X[train_index], X[val_index]
 	y_train, y_val = y[train_index], y[val_index]
 
-#Define Performance Metrics
-#ROC AUC for classification tasks
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import roc_curve
-roc_auc = roc_auc_score(y_val, model.predict(X_val))
-fpr, tpr, thresholds = roc_curve(y_val, model.predict_proba(X_val)[:,1])
-plt.figure()
-plt.plot(fpr, tpr, label='Model (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1],'r--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
-plt.legend(loc="lower right")
-plt.show()
-#Confusion Matrix
-from sklearn.metrics import confusion_matrix
-confusion_matrix(y_val, y_pred)
-#MAE, MSE, RMSE
-from sklearn import metrics
-metrics.mean_absolute_error(y_val, y_pred)
-metrics.mean_squared_error(y_val, y_pred)
-np.sqrt(metrics.mean_squared_error(y_val, y_pred))
-#Classification report
-from sklearn.metrics import classification_report
-print(classification_report(y_val,y_pred))
 
 #Train many quick and dirty models from different categories(e.g., linear, naive Bayes, SVM, Random Forests, neural net, etc.) using standard parameters.
 #########
@@ -474,6 +447,33 @@ models.sort_values(by='Score', ascending=False)
 #Have a quick round of feature selection and engineering.
 #Have one or two more quick iterations of the five previous steps.
 #Short-list the top three to five most promising models, preferring models that make different types of errors.
+#Define Performance Metrics
+#ROC AUC for classification tasks
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+roc_auc = roc_auc_score(y_val, model.predict(X_val))
+fpr, tpr, thresholds = roc_curve(y_val, model.predict_proba(X_val)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Model (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.show()
+#Confusion Matrix
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_val, y_pred)
+#MAE, MSE, RMSE
+from sklearn import metrics
+metrics.mean_absolute_error(y_val, y_pred)
+metrics.mean_squared_error(y_val, y_pred)
+np.sqrt(metrics.mean_squared_error(y_val, y_pred))
+#Classification report
+from sklearn.metrics import classification_report
+print(classification_report(y_val,y_pred))
 
 #Fine-tune the hyperparameters using cross-validation
 #Treat your data transformation choices as hyperparameters, especially when you are not sure about them (e.g., should I replace missing values with zero or with the median value? Or just drop the rows?)
