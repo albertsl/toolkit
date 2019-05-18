d8954ebd7fe76f178a7e1d1bd5a9ebef0f466d36
albertsl <albertsanchezlafuente@gmail.com>
2019-05-18 16:49:21 +0200

    increased pandas visualization limit

--- templates/python for data science.py
+++ templates/python for data science.py
@@ -5,6 +5,9 @@
 #Load packages
 import numpy as np
 import pandas as pd
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
