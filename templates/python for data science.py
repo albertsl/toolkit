c85a03817d26977bbcaf73bb493051d973278c74
Albert <albertsanchezlafuente@gmail.com>
2019-06-04 23:07:29 +0200

    Added ALE plots

--- templates/python for data science.py
+++ templates/python for data science.py
@@ -884,13 +884,9 @@ inter1  =  pdp.pdp_interact(model=model, dataset=X_val, model_features=X_val.col
 pdp.pdp_interact_plot(pdp_interact_out=inter1, feature_names=features_to_plot, plot_type='contour')
 plt.show()
 
-#ALE Plots: faster and unbiased alternative to partial dependence plots (PDPs).
-
-partial dependence plots have a serious problem when the features are correlated.
-
-The computation of a partial dependence plot for a feature that is strongly correlated with other features involves averaging predictions of artificial data instances that are unlikely in reality. This can greatly bias the estimated feature effect.
-
-Next, we consider the second-order effect of humidity and temperature on the predicted number of bikes. Remember that the second-order effect is the additional interaction effect of the two features and does not include the main effects. This means that, for example, you will not see the main effect that high humidity leads to a lower number of predicted bikes on average in the second-order ALE plot.
+#ALE Plots: faster and unbiased alternative to partial dependence plots (PDPs). They have a serious problem when the features are correlated.
+#The computation of a partial dependence plot for a feature that is strongly correlated with other features involves averaging predictions of artificial data instances that are unlikely in reality. This can greatly bias the estimated feature effect.
+https: // github.com/blent-ai/ALEPython
 
 #SHAP Values: Understand how each feature affects every individual prediciton
 import shap
