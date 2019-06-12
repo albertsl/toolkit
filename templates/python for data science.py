dd2285ef5c14dbbe4896adbe1668d61eb8301566
Albert <albertsanchezlafuente@gmail.com>
2019-06-12 22:45:49 +0200

    Udacity course first exercise

--- templates/python for data science.py
+++ templates/python for data science.py
@@ -970,3 +970,61 @@ targetc = KFoldTargetEncoderTrain('column','target',n_fold=5)
 new_df = targetc.fit_transform(df)
 
 new_df[['column_Kfold_Target_Enc','column']]
+
+
+#########
+# Notes on the Pytorch udacity course by Facebook
+#########
+import torch
+torch.__version__
+
+def activation(x):
+    """ Sigmoid activation function #https://upload.wikimedia.org/wikipedia/commons/8/88/Logistic-curve.svg
+    
+        Arguments
+        ---------
+        x: torch.Tensor
+    """
+    return 1/(1+torch.exp(-x))
+
+### Generate some data
+torch.manual_seed(7) # Set the random seed so things are predictable
+# Features are 5 random normal variables
+features = torch.randn((1, 5))
+# True weights for our data, random normal variables again
+weights = torch.randn_like(features)
+# and a true bias term
+bias = torch.randn((1, 1))
+
+y = activation(torch.sum(features * weights) + bias)
+y = activation(torch.mm(features,weights.reshape(5,1))+bias)
+
+#Higher dimension
+### Generate some data
+torch.manual_seed(7) # Set the random seed so things are predictable
+
+# Features are 3 random normal variables
+features = torch.randn((1, 3))
+
+# Define the size of each layer in our network
+n_input = features.shape[1]     # Number of input units, must match number of input features
+n_hidden = 2                    # Number of hidden units 
+n_output = 1                    # Number of output units
+
+# Weights for inputs to hidden layer
+W1 = torch.randn(n_input, n_hidden)
+# Weights for hidden layer to output layer
+W2 = torch.randn(n_hidden, n_output)
+
+# and bias terms for hidden and output layers
+B1 = torch.randn((1, n_hidden))
+B2 = torch.randn((1, n_output))
+
+h = activation(torch.mm(features, W1) + B1)
+y = activation(torch.mm(h, W2) + B2)
+
+#Convert from torch to numpy
+import numpy as np
+a = np.random.rand(4,3)
+b = torch.from_numpy(a)
+b.numpy()
