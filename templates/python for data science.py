7b9484fb5c22397157253eb47ccafa3faa8373ce
Albert Sanchez Lafuente <albertsanchezlafuente@gmail.com>
2019-08-24 14:45:10 +0200

    training neural network

--- templates/python for data science.py
+++ templates/python for data science.py
@@ -1129,3 +1129,33 @@ logits = model(images)
 # Calculate the loss with the logits and the labels
 loss = criterion(logits, labels)
 print(loss)
+
+######Train a  model
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10),
+                      nn.LogSoftmax(dim=1))
+
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.003)
+
+epochs = 5
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vector
+        images = images.view(images.shape[0], -1)
+    
+        # TODO: Training pass
+        optimizer.zero_grad()
+        
+        output = model(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    else:
+        print(f"Training loss: {running_loss/len(trainloader)}")
