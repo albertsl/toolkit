d558bde2702b5b749b28965123688cd8f37004cc
Albert Sanchez Lafuente <albertsanchezlafuente@gmail.com>
2019-08-24 16:13:19 +0200

    load image data

--- templates/python for data science.py
+++ templates/python for data science.py
@@ -1291,3 +1291,48 @@ torch.save(model.state_dict(), 'checkpoint.pth')
 #Load
 state_dict = torch.load('checkpoint.pth')
 model.load_state_dict(state_dict)
+
+######Load image data with Pytorch
+import torch
+from torchvision import datasets, transforms
+import helper
+
+data_dir = 'Cat_Dog_data/train'
+
+transform = transforms.Compose([transforms.Resize(255),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor()])
+dataset = datasets.ImageFolder(data_dir, transform=transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
+
+# Run this to test your data loader
+images, labels = next(iter(dataloader))
+helper.imshow(images[0], normalize=False)
+
+data_dir = 'Cat_Dog_data'
+# Define transforms for the training data and testing data
+train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor()]) 
+
+test_transforms = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor()])
+
+
+# Pass transforms in here, then run the next cell to see how the transforms look
+train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
+test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
+
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=32)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=32)
+
+# change this to the trainloader or testloader 
+data_iter = iter(testloader)
+
+images, labels = next(data_iter)
+fig, axes = plt.subplots(figsize=(10,4), ncols=4)
+for ii in range(4):
+    ax = axes[ii]
+    helper.imshow(images[ii], ax=ax, normalize=False)
