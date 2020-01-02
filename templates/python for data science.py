101dd6dbc0f838cb3051a808c2104bfed478c2dd
Albert Sanchez Lafuente <albertsanchezlafuente@gmail.com>
2020-01-02 21:05:45 +0100

    CNN with TF

--- templates/python for data science.py
+++ templates/python for data science.py
@@ -2155,11 +2155,11 @@ def initialize_parameters():
     Returns:
     parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
     """
-    W1 = tf.get_variable("W1", [25,12288], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    W1 = tf.get_variable("W1", [25,12288], initializer = tf.contrib.layers.xavier_initializer())
     b1 = tf.get_variable("b1", [25,1], initializer = tf.zeros_initializer())
-    W2 = tf.get_variable("W2", [12,25], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    W2 = tf.get_variable("W2", [12,25], initializer = tf.contrib.layers.xavier_initializer())
     b2 = tf.get_variable("b2", [12,1], initializer = tf.zeros_initializer())
-    W3 = tf.get_variable("W3", [6,12], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    W3 = tf.get_variable("W3", [6,12], initializer = tf.contrib.layers.xavier_initializer())
     b3 = tf.get_variable("b3", [6,1], initializer = tf.zeros_initializer())
 
     parameters = {"W1": W1,
@@ -2299,6 +2299,141 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001, num_epochs =
         print ("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
         
         return parameters
+#Forward propagation for CNN
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> FULLYCONNECTED
+    
+    Note that for simplicity and grading purposes, we'll hard-code some values
+    such as the stride and kernel (filter) sizes. 
+    Normally, functions should take these values as function parameters.
+    
+    Arguments:
+    X -- input dataset placeholder, of shape (input size, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "W2"
+                  the shapes are given in initialize_parameters
+
+    Returns:
+    Z3 -- the output of the last LINEAR unit
+    """
+    W1 = parameters['W1']
+    W2 = parameters['W2']
+
+    # CONV2D: stride of 1, padding 'SAME'
+    Z1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME')
+    # RELU
+    A1 = tf.nn.relu(Z1)
+    # MAXPOOL: window 8x8, stride 8, padding 'SAME'
+    P1 = tf.nn.max_pool(A1, ksize = [1, 8, 8, 1], strides = [1, 8, 8, 1], padding='SAME')
+    # CONV2D: filters W2, stride 1, padding 'SAME'
+    Z2 = tf.nn.conv2d(P1, W2, strides=[1, 1, 1, 1], padding='SAME')
+    # RELU
+    A2 = tf.nn.relu(Z2)
+    # MAXPOOL: window 4x4, stride 4, padding 'SAME'
+    P2 = tf.nn.max_pool(A2, ksize = [1, 4, 4, 1], strides = [1, 4, 4, 1], padding='SAME')
+    # FLATTEN
+    F = tf.contrib.layers.flatten(P2)
+    # FULLY-CONNECTED without non-linear activation function (not not call softmax).
+    # 6 neurons in output layer.
+    Z3 = tf.contrib.layers.fully_connected(F, 6, activation_fn=None)
+
+    return Z3
+#Cost for CNN
+def compute_cost(Z3, Y):
+    """
+    Computes the cost
+    
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (number of examples, 6)
+    Y -- "true" labels vector placeholder, same shape as Z3
+    
+    Returns:
+    cost - Tensor of the cost function
+    """
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z3, labels=Y))
+    
+    return cost
+#CNN Model
+def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009,
+          num_epochs = 100, minibatch_size = 64, print_cost = True):
+    """
+    Implements a three-layer ConvNet in Tensorflow:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> FULLYCONNECTED
+    
+    Arguments:
+    X_train -- training set, of shape (None, 64, 64, 3)
+    Y_train -- test set, of shape (None, n_y = 6)
+    X_test -- training set, of shape (None, 64, 64, 3)
+    Y_test -- test set, of shape (None, n_y = 6)
+    learning_rate -- learning rate of the optimization
+    num_epochs -- number of epochs of the optimization loop
+    minibatch_size -- size of a minibatch
+    print_cost -- True to print the cost every 100 epochs
+    
+    Returns:
+    train_accuracy -- real number, accuracy on the train set (X_train)
+    test_accuracy -- real number, testing accuracy on the test set (X_test)
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    (m, n_H0, n_W0, n_C0) = X_train.shape             
+    n_y = Y_train.shape[1]                            
+    costs = []
+    
+    # Create Placeholders of the correct shape
+    X, Y = create_placeholders(n_H0, n_W0, n_C0, n_y)
+    # Initialize parameters
+    parameters = initialize_parameters()
+    # Forward propagation: Build the forward propagation in the tensorflow graph
+    Z3 = forward_propagation(X, parameters)
+    # Cost function: Add cost function to tensorflow graph
+    cost = compute_cost(Z3, Y)
+    # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer that minimizes the cost.
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    # Initialize all the variables globally
+    init = tf.global_variables_initializer()
+     
+    # Start the session to compute the tensorflow graph
+    with tf.Session() as sess:
+        # Run the initialization
+        sess.run(init)
+        # Do the training loop
+        for epoch in range(num_epochs):
+            minibatch_cost = 0.
+            num_minibatches = int(m / minibatch_size) # number of minibatches of size minibatch_size in the train set
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size)
+            for minibatch in minibatches:
+                # Select a minibatch
+                (minibatch_X, minibatch_Y) = minibatch
+                _ , temp_cost = sess.run([optimizer, cost], feed_dict={X:minibatch_X, Y:minibatch_Y})
+                minibatch_cost += temp_cost / num_minibatches
+
+            # Print the cost every epoch
+            if print_cost == True and epoch % 5 == 0:
+                print ("Cost after epoch %i: %f" % (epoch, minibatch_cost))
+            if print_cost == True and epoch % 1 == 0:
+                costs.append(minibatch_cost)
+        
+        # plot the cost
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per tens)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+
+        # Calculate the correct predictions
+        predict_op = tf.argmax(Z3, 1)
+        correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
+        
+        # Calculate accuracy on the test set
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        print(accuracy)
+        train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
+        test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
+        print("Train Accuracy:", train_accuracy)
+        print("Test Accuracy:", test_accuracy)
+                
+        return train_accuracy, test_accuracy, parameters
 #########
 # Deep learning with Pytorch (extracted from the Udacity Secure and Private AI course)
 #########
