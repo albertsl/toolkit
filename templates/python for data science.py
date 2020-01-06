a9b3e15b4f299a373eb206c1d834eb9179ed49b1
Albert Sanchez Lafuente <albertsanchezlafuente@gmail.com>
2020-01-06 18:19:47 +0100

    CNN with Keras

--- templates/python for data science.py
+++ templates/python for data science.py
@@ -2986,6 +2986,14 @@ model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)
 									tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(X_train, Y_train, epochs=100, callbacks=[stopCallback()])
+#Convolutional Model
+model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+									tf.keras.layers.MaxPooling2D(2, 2),
+									tf.keras.layers.Flatten(),
+									tf.keras.layers.Dense(128, activation='relu'),
+									tf.keras.layers.Dense(10, activation='softmax')])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(X_train, Y_train, epochs=10, callbacks=[stopCallback()])
 #########
 # Deep learning with Pytorch (extracted from the Udacity Secure and Private AI course)
 #########
